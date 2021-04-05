806ef517563fc41adab95ad4203b775e344896f3
Shady Ganem <shady.ganem@intel.com>
2021-04-05 19:56:33 +0300

    deleted refrence code starting from scartch

---
+++ libIntec/tests/testlibusb.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <libusb-1.0/libusb.h>
+
+#define test_main main
+
+
+int inti_libusb()
+{
+	;
+}
+
+int test_main(int argc, char** argv)
+{
+	std::cout << "running libusbtest" << std::endl;
+	libusb_device **devs;
+	libusb_context *context = NUll;
+
+	return 0;
+}
