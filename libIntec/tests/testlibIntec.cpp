641a2cf4292efd6aaa268280c94c455e6cc8d9e7
Shady Ganem <shady.ganem@intel.com>
2021-04-08 14:17:19 +0300

    read and write functions are implemented

--- libIntec/tests/testlibIntec.cpp
+++ libIntec/tests/testlibIntec.cpp
@@ -11,17 +11,35 @@
 
 int test_main(int argc, char ** argvv)
 {
-	std::cout << "libIntec test" << std::endl;
+	std::cout << "libIntec API test" << std::endl;
 	IntecUsbDeviceType DevType = IntecH;
 	if (libIntec_Initialize(DevType) != STATUS_OK)
 	{
-		std::cout << "libIntec driver initialization failed" << std::endl;
+		std::cout << "TEST FAIL: libIntec driver initialization failed" << std::endl;
+		libIntec_Exit();
 		exit(1);
+	}
+	else
+	{
+		std::cout << "TEST PASS - llibIntec_Initialize" << std::endl;
 	}
 
 	int num_of_devices;
-	libtIntec_GetNumOfUsbDevices(num_of_devices);
-	std::cout << "number of devices found on this machine " << num_of_devices << std::endl;
+	if(libtIntec_GetNumOfUsbDevices(num_of_devices) != STATUS_OK)
+	{
+		std::cout << "TEST FAIL: GetNumOfUsbDevices returned error" << std::endl;
+		libIntec_Exit();
+		exit(1);
+	}
+	else
+	{
+		std::cout << "TEST PASS - libtIntec_GetNumOfUsbDevices" << std::endl;
+		std::cout << "number of devices found on this machine " << num_of_devices << std::endl;
+	}
+
+
+
+
 	libIntec_Exit();
 	return 0;
 }
